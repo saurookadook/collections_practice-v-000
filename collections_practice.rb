df7a439153f2002f8314d9c383c78984e288d764
Andy Maskiell <maskiella@gmail.com>
2017-12-11 00:27:39 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,74 @@
+require 'pry'
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif b < a
+      -1
+    elsif b > a
+      1
+    end
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
+    end
+  end
+end
+
+def swap_elements(array)
+  # binding.pry
+  array.sort do |a, b|
+    if a == array[1]
+      1
+    elsif b == array[2]
+      -1
+    end
+  end
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  kesha_array = []
+  array.each do |name|
+    # binding.pry
+    name[2] = "$"
+    kesha_array << name
+  end
+  kesha_array
+end
+
+def find_a(array)
+  array.select do |word|
+    word.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  until array.length == 1
+    array.sort do |a, b|
+      a + b
+    end
+end
+
+# def sum_array(array)
+#   until array.length == 1
+#     array[1] = (array[1] + array[0])
+#     array.shift
+#   end
+# end
